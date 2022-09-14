ebe83506b96a2932d0f370c1edc598983ccc02d9
Procentaurus <michalski.ob@gmail.com>
2022-09-14 23:56:13 +0200

    Dodanie STATIC_ROOT

--- YourPocket/settings.py
+++ YourPocket/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
@@ -126,7 +127,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images/user_images' )
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images/user_images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
